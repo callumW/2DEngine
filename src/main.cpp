ad7bee0101ebbebc52b9062b5c257965572fd107
Callum Wilson <calwilson@live.co.uk>
2020-04-24 11:45:59 +0100

    Draw G on screen

--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,42 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("terminal-reboot", SDL_WINDOWPOS_UNDEFINED,
-                                          SDL_WINDOWPOS_UNDEFINED, 600, 400, SDL_WINDOW_VULKAN);
+    SDL_Window* window =
+        SDL_CreateWindow("terminal-reboot", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600,
+                         400, SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN);
 
     if (window == nullptr) {
         std::cout << "failed to create window: " << SDL_GetError() << std::endl;
     }
     else {
+
+        SDL_Surface* surface = SDL_GetWindowSurface(window);
+
+        SDL_FillRect(surface, nullptr, 0xffffff);
+
+        if (surface) {
+            SDL_Surface* img_surface = SDL_LoadBMP("./G.bmp");
+            if (img_surface == nullptr) {
+                std::cout << "Failed to load bitmap: " << SDL_GetError() << std::endl;
+            }
+            else {
+                if (0 != SDL_BlitSurface(img_surface, nullptr, surface, nullptr)) {
+                    std::cout << "Failed to blit surface" << std::endl;
+                }
+                else {
+                    SDL_UpdateWindowSurface(window);
+                }
+            }
+        }
+        else {
+            std::cout << "Failed to load window surface: " << SDL_GetError() << std::endl;
+        }
+
         SDL_Event e;
         bool quit = false;
         while (!quit) {
             while (SDL_PollEvent(&e)) {
                 if (e.type == SDL_QUIT) {
-                    quit = true;
-                }
-                if (e.type == SDL_KEYDOWN) {
-                    quit = true;
-                }
-                if (e.type == SDL_MOUSEBUTTONDOWN) {
                     quit = true;
                 }
             }
