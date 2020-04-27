4c310943c52458611ec8d22f2c1a3aa7b9121a91
Callum Wilson <calwilson@live.co.uk>
2020-04-27 23:02:37 +0100

    move test build up to main makefile

--- Makefile
+++ Makefile
@@ -4,6 +4,8 @@ COMPILER=clang++
 CPP_STD=c++14
 
 BUILD_DIR=build/
+TEST_DIR=tests/
+TEST_BUILD_DIR=${BUILD_DIR}/tests/
 
 # Main Source Files
 SRC_FILES=$(wildcard src/*.cpp)
@@ -11,6 +13,21 @@ SRC_FILES=$(wildcard src/*.cpp)
 OBJ=$(patsubst %.cpp,%.o, ${SRC_FILES})
 
 DEPS=$(patsubst %.cpp,%.d, ${SRC_FILES})
+
+# Test source files
+TEST_SRC_FILES=$(wildcard tests/src/*.cpp)
+
+FILES_UNDER_TEST= \
+src/game_math.cpp \
+src/EntityManager.cpp
+
+TEST_OBJ=$(patsubst %.cpp,%.o, ${TEST_SRC_FILES})
+
+OBJS_UNDER_TEST=$(patsubst %.cpp,%.o, ${FILES_UNDER_TEST})
+
+TEST_DEPS=$(patsubst %.cpp,%.d, ${TEST_SRC_FILES})
+
+TEST_INCLUDES_FILES=$(wildcard tests/include/*.h)
 
 ASSET_FILES=$(wildcard assets/*)
 
@@ -31,8 +48,11 @@ BINARY_NAME=terminal-reboot-${VERSION}.app
 
 OUTPUT=${BUILD_DIR}${BINARY_NAME}
 
+TEST_APP=run_tests.app
+TEST_OUTPUT=${TEST_BUILD_DIR}${TEST_APP}
+
 .PHONY: all
-all: format-code build-app
+all: format-code build-app tests
 
 .PHONY: build-app
 build-app: ${OUTPUT}
@@ -45,6 +65,7 @@ ${OUTPUT}: ${BUILD_DIR} ${OBJ} ${BUILD_ASSETS}
 
 # Include generated dependency files
 -include ${DEPS}
+-include ${TEST_DEPS}
 
 ${BUILD_DIR}:
 	mkdir ${BUILD_DIR}
@@ -56,7 +77,17 @@ clean:
 
 .PHONY: format-code
 format-code:
-	clang-format --style=file -i ${SRC_FILES} ${INCLUDE_FILES}
+	clang-format --style=file -i ${SRC_FILES} ${INCLUDE_FILES} ${TEST_SRC_FILES} ${TEST_INCLUDES_FILES}
 
 ${BUILD_ASSETS}:
 	cp -r assets ${BUILD_DIR}/assets
+
+.PHONY: tests
+tests: ${TEST_OUTPUT}
+	./${TEST_OUTPUT}
+
+${TEST_OUTPUT}: ${TEST_BUILD_DIR} ${TEST_OBJ}
+	${COMPILER} -std=${CPP_STD} ${TEST_OBJ} ${OBJS_UNDER_TEST} ${LD_FLAGS} -o ${TEST_OUTPUT}
+
+${TEST_BUILD_DIR}:
+	mkdir ${TEST_BUILD_DIR}
--- tests/src/test_maths.cpp
+++ tests/src/test_maths.cpp
@@ -16,24 +16,7 @@ static bool approx(float a, float b)
 
 int main()
 {
-    // Test Clamp func
-    // if (!approx(clamp_trig_arg(-2.2f), -0.2f)) {
-    //     std::cout << "clamp_trig_arg failed " << __func__ << ":" << __LINE__ << " "
-    //               << clamp_trig_arg(-2.2f) << std::endl;
-    // }
-    // if (!approx(clamp_trig_arg(-2.0f), 0.0f)) {
-    //     std::cout << "clamp_trig_arg failed " << __func__ << ":" << __LINE__ << " "
-    //               << clamp_trig_arg(-2.0f) << std::endl;
-    // }
-    // if (!approx(clamp_trig_arg(2.0f), 0.0f)) {
-    //     std::cout << "clamp_trig_arg failed " << __func__ << ":" << __LINE__ << " "
-    //               << clamp_trig_arg(2.0f) << std::endl;
-    // }
-    // if (!approx(clamp_trig_arg(2.9f), 0.9f)) {
-    //     std::cout << "clamp_trig_arg failed " << __func__ << ":" << __LINE__ << " "
-    //               << clamp_trig_arg(2.9f) << std::endl;
-    // }
-
+    int failed_tests = 0;
     vec2f_t x_axis{0.0f, 1.0f};
     vec2f_t y_axis{1.0f, 0.0f};
 
@@ -44,20 +27,22 @@ int main()
     if (!approx(y_axis.inner_angle(x_axis), 90.0f)) {
         std::cout << "angle failed " << __func__ << ":" << __LINE__ << " "
                   << y_axis.inner_angle(x_axis) << std::endl;
+        failed_tests++;
     }
 
     vec2f_t neg_x(0.0f, -1.0f);
     if (!approx(neg_x.angle(), -90.0f)) {
-        std::cout << "angle failed " << __func__ << ":" << __LINE__ << " "
-                  << neg_x.angle() << std::endl;
+        std::cout << "angle failed " << __func__ << ":" << __LINE__ << " " << neg_x.angle()
+                  << std::endl;
+        failed_tests++;
     }
 
     vec2f_t neg_y(-1.0f, 0.0f);
     if (!approx(neg_y.angle(), 180.0f)) {
-        std::cout << "angle failed " << __func__ << ":" << __LINE__ << " "
-                  << neg_y.angle() << std::endl;
+        std::cout << "angle failed " << __func__ << ":" << __LINE__ << " " << neg_y.angle()
+                  << std::endl;
+        failed_tests++;
     }
 
-
-    return 0;
+    return failed_tests;
 }
